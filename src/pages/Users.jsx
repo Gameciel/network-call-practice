7fe574bc9e1d3957bd506545e7267af95bf7c119
Gameciel <christofer.calvin.pranata@gmail.com>
2023-03-11 15:29:40 +0700

    adjusted password font styling for clarity

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -8,73 +8,6 @@ import { Row } from "primereact/row";
 import { API_URL } from "../fake-api/API_URL";
 
 export default function Users() {
-	const [userList, setUserList] = useState([]);
-
-	useEffect(() => {
-		axios
-			.get(`${API_URL}/user`)
-			.then(res => {
-				setUserList(res.data);
-			})
-			.catch(err => {
-				console.log(err.response);
-			});
-	}, []);
-
-	const GenerateFetchedUserData = () => {
-		const statusBodyTemplate = user => {
-			return <Tag value={user.role} severity={getSeverity(user)}></Tag>;
-		};
-
-		const getSeverity = user => {
-			switch (user.role) {
-				case "user":
-					return "info";
-
-				case "admin":
-					return "danger";
-
-				default:
-					return null;
-			}
-		};
-
-		const footerGroup = (
-			<ColumnGroup>
-				<Row>
-					<Column
-						footer="Total User:"
-						colSpan={0}
-						footerStyle={{ textAlign: "left" }}
-					/>
-					<Column footer={userList.length} />
-					<Column colSpan={4} />
-				</Row>
-			</ColumnGroup>
-		);
-		return (
-			<div className="card">
-				<DataTable
-					value={userList}
-					size={"large"}
-					stripedRows
-					paginator
-					sortMode="single"
-					rows={5}
-					rowsPerPageOptions={[5, 10]}
-					footerColumnGroup={footerGroup}
-					tableStyle={{ minWidth: "50rem" }}
-				>
-					<Column field="id" sortable header="ID"></Column>
-					<Column field="user_name" header="Name"></Column>
-					<Column field="email" header="E-mail"></Column>
-					<Column field="password" header="Password"></Column>
-					<Column header="Role" body={statusBodyTemplate}></Column>
-				</DataTable>
-			</div>
-		);
-	};
-
 	return (
 		<div className="d-flex flex-column container align-items-center mb-5 pb-5">
 			<div class="alert alert-info alert-dismissible fade show" role="alert">
@@ -99,3 +32,74 @@ export default function Users() {
 		</div>
 	);
 }
+
+const GenerateFetchedUserData = () => {
+	const [userList, setUserList] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(`${API_URL}/user`)
+			.then(res => {
+				setUserList(res.data);
+			})
+			.catch(err => {
+				console.log(err.response);
+			});
+	}, []);
+
+	const statusBodyTemplate = user => {
+		return <Tag value={user.role} severity={getSeverity(user)}></Tag>;
+	};
+
+	const getSeverity = user => {
+		switch (user.role) {
+			case "user":
+				return "info";
+
+			case "admin":
+				return "danger";
+
+			default:
+				return null;
+		}
+	};
+
+	const footerGroup = (
+		<ColumnGroup>
+			<Row>
+				<Column
+					footer="Total User:"
+					colSpan={0}
+					footerStyle={{ textAlign: "left" }}
+				/>
+				<Column footer={userList.length} />
+				<Column colSpan={4} />
+			</Row>
+		</ColumnGroup>
+	);
+	return (
+		<div className="card">
+			<DataTable
+				value={userList}
+				size={"large"}
+				stripedRows
+				paginator
+				sortMode="single"
+				rows={5}
+				rowsPerPageOptions={[5, 10]}
+				footerColumnGroup={footerGroup}
+				tableStyle={{ minWidth: "50rem" }}
+			>
+				<Column field="id" sortable header="ID"></Column>
+				<Column field="user_name" header="Name"></Column>
+				<Column field="email" header="E-mail"></Column>
+				<Column
+					field="password"
+					className="font-monospace fw-bold"
+					header="Password"
+				></Column>
+				<Column header="Role" body={statusBodyTemplate}></Column>
+			</DataTable>
+		</div>
+	);
+};
